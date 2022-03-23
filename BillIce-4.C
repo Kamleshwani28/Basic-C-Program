32b4ec8fb9bdfdd50d4b916eed098c9f38347687
Kamlesh wani <101851855+Kamleshwani28@users.noreply.github.com>
2022-03-23 18:25:09 +0530

    Billce-4.C
    ADDED:[Kamlesh]Program for Bills or Price

---
+++ BillIce-4.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<conio.h>hgyg
+void main()
+{
+	int icecream, quantity, bill;
+	float discount_percentage = 15;
+	float discounted_bill;
+	clrscr();
+	printf("\nEnter the Icecream Price ::");
+	scanf("%d", &icecream);
+	printf("\nEnter the Quantity :: ");
+	scanf("%d", &quantity);
+	
+	bill = icecream * quantity;
+	
+	printf("\nTotal bill is = %d", bill);
+	printf("\nDiscount Percentage is =%.2f", discount_percentage);
+
+	discount_percentage = (discount_percentage * bill) / 100;
+
+	discounted_bill = (bill - discount_percentage);
+
+	printf("\nDiscount Amount = %.2f\n", discount_percentage);
+	printf("\nFinal Bill::-\n");
+      printf("\nTotal Net Bill  is =%.2f", discounted_bill);
+
+	getch();
+}
