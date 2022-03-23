5464aa15783a15ccc0a76fdead325c0ec3266a2d
Kamlesh wani <101851855+Kamleshwani28@users.noreply.github.com>
2022-03-23 18:40:50 +0530

    Update and rename math2.C to Addtwono.C

---
+++ Addtwono.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+      int a,b,c=0;
+      clrscr();
+      printf("\nEnter Two Number::");
+      scanf("%d%d",&a,&b);
+      c=a+b;
+      printf("\nTotal Addition=%d",c);
+      getch();      
+}
