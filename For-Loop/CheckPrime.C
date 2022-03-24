74c984d11cac7bef9034863a5c99b5a4d368cf45
Kamleshwani28 <kamleshlonkheda@gmail.com>
2022-03-24 09:39:52 +0530

    ADDED:[Kamlesh] Program for Check Prime Number or Not

---
+++ For-Loop/CheckPrime.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,i,flag=1;
+	clrscr();
+	printf("\nEnter the No:");
+	scanf("%d",&n);
+	if(n==1)
+	{
+		flag=1;
+	}
+	else
+	{
+		for (i=2;i<n;i++)
+		{
+			if(n%2==0)
+			{
+				flag=0;
+				break;
+			}
+		}
+	}
+	if (flag==1)
+	{
+		printf("\nNo is prime");
+	}
+	else
+	{
+		printf("\nNo is Not prime");
+	}
+	getch();
+}
