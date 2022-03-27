9d5841d235b63ccc65f7f407168e892fd045f1a4
Kamlesh wani <101851855+Kamleshwani28@users.noreply.github.com>
2022-03-27 16:54:43 +0530

    ADDED:[Kamlesh] Program for  Marks Sheet

--- Simple-program/Marks.C
+++ Simple-program/Marks.C
@@ -1,47 +1,23 @@
 #include<stdio.h>
-
 #include<conio.h>
-
 void main()
-
 {
-     
-	int english,marathi,hindi,physics,chemistry;
-    
+	int english,marathi,hindi,physics,chemistry,m;
 	float p;
-    
-	int m;
-    
 	clrscr();
-    
-	printf("\n Marks of English::");
-    
+	printf("\n Marks of English ::");
 	scanf("%d",&english);
-    
-	printf("\n Marks of Marathi::");
-    
+	printf("\n Marks of Marathi ::");
 	scanf("%d",&marathi);
-    
-	printf("\n Marks of Hindi::");
-    
+	printf("\n Marks of Hindi ::");
 	scanf("%d",&hindi);
-    
-	printf("\n Marks of Physics::");
-    
+	printf("\n Marks of Physics ::");
 	scanf("%d",&physics);
-    
-	printf("\n Marks of Chemistry::");
-    
+	printf("\n Marks of Chemistry ::");
 	scanf("%d",&chemistry);
-    
 	m=(english+marathi+hindi+physics+chemistry);
-    
-	printf("\n Total Marks Often By=%d/500",m);
-    
+	printf("\n\n Total Marks Often By=%d/500",m);
 	p=m/5;
-    
 	printf("\n Total Persentage=%.2f",p);
-    
 	getch();
-
 }
