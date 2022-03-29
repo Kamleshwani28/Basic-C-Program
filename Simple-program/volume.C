fed008f40ada102fef7437b6c868dbe81795b623
Kamlesh wani <101851855+Kamleshwani28@users.noreply.github.com>
2022-03-29 20:45:52 +0530

    ADDED:[Kamlesh] Program for Finding Volume

---
+++ Simple-program/volume.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+       int l,b,h,v;
+       clrscr();
+       printf("\nEnter Te Value of Length :");
+       scanf("%d",&l);
+       printf("\nEnter The Valude of breadth :");
+       scanf("%d",&b);
+       printf("\nEner The Valude of Hight :");
+       scanf("%d",&h);
+       v=l*b*h;
+       printf("\n\nVolume of Cube ::%d",v);
+       getch();
+            
+}
