1006571f6f5ad01d0ea2aec65d7a9e0e7d45f309
Kamlesh wani <101851855+Kamleshwani28@users.noreply.github.com>
2022-04-06 22:21:03 +0530

    ADDED:[Kamlesh] Program for Print Patten

---
+++ For-Loop/printpattern.C
@@ -0,0 +1,18 @@
+// print pattern
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+	clrscr();
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	getch();
+}
